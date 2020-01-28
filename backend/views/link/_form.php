52daf31aed5d53a5882208d47227781a8e0ea712
Maxim Gabidullin <after@ya.ru>
2020-01-28 06:30:16 +0300

    add drop down lists

--- backend/views/link/_form.php
+++ backend/views/link/_form.php
@@ -1,24 +1,33 @@
 <?php
 
+use common\models\LinkBlock;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\Link */
 /* @var $form yii\widgets\ActiveForm */
+
+$linkBlocks = LinkBlock::find()->select(['name', 'id'])->indexBy('id')->column();
+$private = [
+    false => Yii::t('app', 'Yes'),
+    true  => Yii::t('app','No'),
+];
 ?>
 
 <div class="link-form">
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'link_block_id')->textInput() ?>
+    <?= $form->field($model, 'link_block_id')->dropdownList($linkBlocks, [
+            'prompt' => Yii::t('app', 'Select Block')
+    ]) ?>
 
     <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'href')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'private')->textInput() ?>
+    <?= $form->field($model, 'private')->dropDownList($private) ?>
 
     <?= $form->field($model, 'icon')->textInput(['maxlength' => true]) ?>
 
